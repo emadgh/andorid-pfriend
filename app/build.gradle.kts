b8c9e42234c2228047a027f576b159ed6de4cbb8
Emad Ghasemi <emadghasemi_2006@yahoo.com>
2026-08-14 13:27:48 +0330

    fix(android): migrate AGP 9 to built-in Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -29,7 +28,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
